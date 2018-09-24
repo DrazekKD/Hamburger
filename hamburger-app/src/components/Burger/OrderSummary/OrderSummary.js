80c13b6e903695e0616e2d6d08b1f0007ee87155
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2018-09-24 11:40:19 +0200

    refactor func for class

--- hamburger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ hamburger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Auxs';
 import Button from "../../UI/Button/Button";
 
-const OrderSummary = (props) => {
-	let ingredients = Object.keys(props.ingredients)
-			.map((keyIng) =>{
+class OrderSummary extends Component {
+	render() {
+		let ingredients = Object.keys(this.props.ingredients)
+			.map((keyIng) => {
 				return (
 					<li key={keyIng}>
-						<strong style={{textTransform: 'capitalize'}}>{keyIng}</strong>: {props.ingredients[keyIng]}
+						<strong style={{textTransform: 'capitalize'}}>{keyIng}</strong>: {this.props.ingredients[keyIng]}
 					</li>
 				)
 			});
 
+		return (
+			<Aux>
+				<h3>Your Order</h3>
+				<p>a delicious burger with the fallowing ingredients:</p>
+				<ul>
+					{ingredients}
+				</ul>
+				<p><strong>Total price: {this.props.totalPrice.toFixed(2)}</strong></p>
+				<p>Continue to Checkout?</p>
+				<Button clicked={this.props.cancel} btnType="Danger">CENCEL</Button>
+				<Button clicked={this.props.continue} btnType="Success">CONTINUE</Button>
+			</Aux>
 
-	return (
-		<Aux>
-			<h3>Your Order</h3>
-			<p>a delicious burger with the fallowing ingredients:</p>
-			<ul>
-				{ingredients}
-			</ul>
-			<p><strong>Total price: {props.totalPrice.toFixed(2)}</strong></p>
-			<p>Continue to Checkout?</p>
-			<Button clicked={props.cancel} btnType="Danger">CENCEL</Button>
-			<Button clicked={props.continue} btnType="Success">CONTINUE</Button>
-		</Aux>
-	)
-
-};
+		);
+	}
+}
 
 export default OrderSummary;
